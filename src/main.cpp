2c1cf4d76eca52694c5e7176267e1dd14a0d653a
SamuelFVG2 <felipegoncalvesbr2@gmail.com>
2026-06-11 21:46:19 -0300

    feat: add archive reading funcion, change code execution path

--- src/main.cpp
+++ src/main.cpp
@@ -2,68 +2,50 @@
 
 #include "classes/date/date.h"
 #include "classes/person/person.h"
+#include <fstream>
 
-Person getPersonFromLine(std::string line)
-{
+Person getPersonFromLine(std::string line) {
     std::string name = "", numbers = "";
 
     for (char c : line) {
         if (isdigit(c)) {
             numbers += c;
-        }
-        else {
+        } else {  
             name += c;
         }
     }
 
     Person person(name);
 
-    for (int i = 0; i < numbers.size(); i += 6) {
+    for (int i = 0; i < (int)numbers.size(); i += 6) {
         std::string dateString = numbers.substr(i, 6);
 
-        int day = std::stoi(dateString.substr(0, 2)), month = std::stoi(dateString.substr(2, 2)), year = std::stoi(dateString.substr(4, 2));
+        int day   = std::stoi(dateString.substr(0, 2));
+        int month = std::stoi(dateString.substr(2, 2));
+        int year  = std::stoi(dateString.substr(4, 2));
 
-        person.addDate(
-            Date(day, month, year)
-        );
+        person.addDate(Date(day, month, year));
     }
 
     return person;
 }
 
-int main()
-{
-    Person person =
-        getPersonFromLine("JOAQUIM310799060605150280");
 
-    std::cout << person.getPersonString();
+int main() {
+    std::vector<Person> persons;
+    std::ifstream file("./arquivo.txt");
+    std::string line;
+
+    while (std::getline(file, line)) {
+        Person person = getPersonFromLine(line);
+        persons.push_back(person);
+    }
+    std::cout << persons.size() << std::endl;
+    for (int i = 0; i<persons.size();i++){
+        std::cout << persons[i].getPersonString() << std::endl;
+    } 
+
+    file.close();
 
     return 0;
 }
-// #include <iostream>
-// #include <fstream>
-// #include <sstream>
-// #include <vector>
-// #include <string>
-
-
-// std::vector<std::string> getPersonFromLine(std::string line ) {
-//   std::string cell, aux;
-//   std::vector<std::string> data(4);
-//   std::stringstream ss(line);
-
-// }
-
-// int main()
-// {
-//   std::ifstream file("./dados.txt");
-//   std::string line;
-//   std::getline(file, line);
-
-//   while (std::getline(file, line)) { 
-//     std::vector<std::string> person = getPersonFromLine(line);
-//   }
-
-//   return 0;
-// }
-
